955e6dad42b9dab4b62852a26215cc6abb92374b
Manaswi Patil <manaswi.c.patil@gmail.com>
2021-03-03 22:53:45 +1300

    New Contact form

---
+++ PhoneBook/NewContact.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace PhoneBook
+{
+    partial class NewContact
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.newContactLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.phoneNumberLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.addContactButten = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.headerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.AutoSize = true;
+            this.headerPanel.BackColor = System.Drawing.Color.White;
+            this.headerPanel.Controls.Add(this.newContactLabel);
+            this.headerPanel.Location = new System.Drawing.Point(2, 3);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(597, 88);
+            this.headerPanel.TabIndex = 0;
+            // 
+            // newContactLabel
+            // 
+            this.newContactLabel.AutoSize = true;
+            this.newContactLabel.Font = new System.Drawing.Font("Source Sans Pro SemiBold", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newContactLabel.Location = new System.Drawing.Point(242, 27);
+            this.newContactLabel.Name = "newContactLabel";
+            this.newContactLabel.Size = new System.Drawing.Size(112, 24);
+            this.newContactLabel.TabIndex = 0;
+            this.newContactLabel.Text = "New Contact";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNameLabel.Location = new System.Drawing.Point(130, 117);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(86, 22);
+            this.firstNameLabel.TabIndex = 1;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNameLabel.Location = new System.Drawing.Point(130, 164);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(85, 22);
+            this.lastNameLabel.TabIndex = 2;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // phoneNumberLabel
+            // 
+            this.phoneNumberLabel.AutoSize = true;
+            this.phoneNumberLabel.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneNumberLabel.Location = new System.Drawing.Point(130, 208);
+            this.phoneNumberLabel.Name = "phoneNumberLabel";
+            this.phoneNumberLabel.Size = new System.Drawing.Size(115, 22);
+            this.phoneNumberLabel.TabIndex = 3;
+            this.phoneNumberLabel.Text = "Phone Number";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressLabel.Location = new System.Drawing.Point(130, 270);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(65, 22);
+            this.addressLabel.TabIndex = 4;
+            this.addressLabel.Text = "Address";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailLabel.Location = new System.Drawing.Point(130, 336);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(50, 22);
+            this.emailLabel.TabIndex = 5;
+            this.emailLabel.Text = "Email";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNameTextBox.Location = new System.Drawing.Point(264, 117);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(206, 29);
+            this.firstNameTextBox.TabIndex = 6;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNameTextBox.Location = new System.Drawing.Point(264, 161);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(206, 29);
+            this.lastNameTextBox.TabIndex = 7;
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(264, 205);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(206, 29);
+            this.phoneNumberTextBox.TabIndex = 8;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressTextBox.Location = new System.Drawing.Point(264, 249);
+            this.addressTextBox.Multiline = true;
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(206, 69);
+            this.addressTextBox.TabIndex = 9;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTextBox.Location = new System.Drawing.Point(264, 333);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(206, 29);
+            this.emailTextBox.TabIndex = 10;
+            // 
+            // addContactButten
+            // 
+            this.addContactButten.BackColor = System.Drawing.Color.White;
+            this.addContactButten.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addContactButten.Location = new System.Drawing.Point(134, 403);
+            this.addContactButten.Name = "addContactButten";
+            this.addContactButten.Size = new System.Drawing.Size(163, 55);
+            this.addContactButten.TabIndex = 11;
+            this.addContactButten.Text = "Add Contact";
+            this.addContactButten.UseVisualStyleBackColor = false;
+            this.addContactButten.Click += new System.EventHandler(this.addContactButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.White;
+            this.exitButton.Font = new System.Drawing.Font("Source Sans Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(303, 403);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(163, 55);
+            this.exitButton.TabIndex = 12;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // NewContact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 500);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.addContactButten);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.phoneNumberLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.headerPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NewContact";
+            this.Text = "NewContact";
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel headerPanel;
+        private System.Windows.Forms.Label newContactLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label phoneNumberLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Button addContactButten;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
---
+++ PhoneBook/NewContact.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PhoneBook
+{
+    public partial class NewContact : Form
+    {
+        SqlConnection sqlconn;
+        public NewContact()
+        {
+            InitializeComponent();
+        }
+
+        public NewContact(SqlConnection sqlConn)
+            :this()
+        {
+            this.sqlconn = sqlConn;
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void addContactButton_Click(object sender, EventArgs e)
+        {
+            sqlconn.Open();
+            string sqlquery = "Insert into Contacts (FirstName, LastName, PhoneNumber, Address, Email)" +
+                "values ('"+ firstNameTextBox.Text+"','"+ lastNameTextBox.Text+"','"+ phoneNumberTextBox.Text+"','"+ addressTextBox.Text+"','"+ emailTextBox.Text+"')";
+
+            SqlCommand sqlcmd = new SqlCommand(sqlquery, sqlconn);
+
+            sqlcmd.ExecuteNonQuery();
+            
+            sqlconn.Close();
+
+            foreach(Control c in Controls)
+            {
+                if (c is TextBox)
+                    ((TextBox)c).Text = String.Empty;
+            }
+
+            MessageBox.Show("Contact added Successfully. Please refresh the contact list.");
+        }
+    }
+}
